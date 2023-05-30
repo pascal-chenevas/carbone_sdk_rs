3888a5e6288f1ffd4f9d869fc661866b71310844
pascal-chenevas <pcp.dev@posteo.de>
2023-05-30 21:40:09 +0200

    impl tests test_generate_template_*

--- tests/carbone_test.rs
+++ tests/carbone_test.rs
@@ -12,7 +12,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_init_sdk_error_missing_token() -> Result<(), CarboneSdkError> {
+    fn test_init_sdk_error_missing_token() {
 
         let config = Default::default();
     
@@ -25,7 +25,6 @@ mod tests {
         
         assert_eq!(expected_error.to_string(), error);
 
-        Ok(())
     }
 
     #[test]
@@ -43,6 +42,97 @@ mod tests {
         Ok(())
 
     }
+
+    #[test]
+    fn test_generate_template_id_odt_2_payload_1() -> Result<(), CarboneSdkError> {
+
+        let mut config: Config = Default::default();
+        config.api_token = "test_a".to_string();
+        let cabone_sdk = CarboneSDK::new(config)?;
+
+        let file_name = "tests/template.test.odt".to_string();
+        let template_id = cabone_sdk.generate_template_id(&file_name, "ThisIsAPayload")?;
+
+        let expected_template_id = "7de8d1d8676abb32291ea5119cb1f78fe37fdfdc75332fcdae28f1e30d064ac0".to_string();
+        assert_eq!(expected_template_id, template_id);
+        
+        Ok(())
+    }
+
+    #[test]
+    fn test_generate_template_id_odt_3_payload_2() -> Result<(), CarboneSdkError> {
+
+        let mut config: Config = Default::default();
+        config.api_token = "test_a".to_string();
+        let cabone_sdk = CarboneSDK::new(config)?;
+
+        let file_name = "tests/template.test.odt".to_string();
+        let template_id = cabone_sdk.generate_template_id(&file_name, "8B5PmafbjdRqHuksjHNw83mvPiGj7WTE")?;
+
+        let expected_template_id = "a62eb407a5d5765ddf974636de8ab47bda7915cebd61197d7a2bb42ae70ffcd6".to_string();
+        assert_eq!(expected_template_id, template_id);
+        
+        Ok(())
+    }
+
+
+    #[test]
+    fn test_generate_template_id_html_1() -> Result<(), CarboneSdkError> {
+
+        let mut config: Config = Default::default();
+        config.api_token = "test_a".to_string();
+        let cabone_sdk = CarboneSDK::new(config)?;
+
+        let file_name = "tests/template.test.html".to_string();
+        let template_id = cabone_sdk.generate_template_id(&file_name, "")?;
+
+        let expected_template_id = "75256dd5c260cdf039ae807d3a007e78791e2d8963ea1aa6aff87ba03074df7f".to_string();
+        assert_eq!(expected_template_id, template_id);
+        
+        Ok(())
+
+    }  
+
+    #[test]
+    fn test_generate_template_id_html_2_payload_1() -> Result<(), CarboneSdkError> {
+
+        let mut config: Config = Default::default();
+        config.api_token = "test_a".to_string();
+        let cabone_sdk = CarboneSDK::new(config)?;
+
+        let file_name = "tests/template.test.html".to_string();
+        let payload = "This is a long payload with different characters 1 *5 &*9 %$ 3%&@9 @(( 3992288282 29299 9299929";
+        let template_id = cabone_sdk.generate_template_id(&file_name, payload)?;
+
+        let expected_template_id = "70799b421cc9cf75d9112273a8e054c141d484eb8d5988bd006fac83e3990707".to_string();
+        assert_eq!(expected_template_id, template_id);
+        
+        Ok(())
+
+    }  
+
+    #[test]
+    fn test_generate_template_id_error() -> Result<(), CarboneSdkError> {
+
+        let mut config: Config = Default::default();
+        config.api_token = "test_a".to_string();
+        let cabone_sdk = CarboneSDK::new(config)?;
+
+        let file_name = "".to_string();
+        let payload = "";
+       
+        let error = match cabone_sdk.generate_template_id(&file_name, payload) {
+            Ok(_) => panic!("the function doesn't return an error"),
+            Err(e) => e.to_string(),
+        };
+
+        let expected_error = CarboneSdkError::MissingArgument("generate_template_id".to_string(), "template_file_name".to_string()); 
+
+        assert_eq!(expected_error.to_string(), error);
+
+        Ok(())
+
+    }  
 
     #[test]
     fn test_add_template() -> Result<(), CarboneSdkError> {
@@ -71,7 +161,7 @@ mod tests {
 
         let config = Config{
             api_token: "test_q".to_string(),
-            api_url: format!("{}{}", "http://127.0.0.1:", server.port()), // port change each run
+            api_url: format!("{}{}", "http://127.0.0.1:", server.port()), // port changes each run
             api_timeout: 4,
             api_version: "2".to_string(),
         };
