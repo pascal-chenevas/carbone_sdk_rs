37a253ead068cc3a6d326ed2350b17997ad26ada
pascal-chenevas <pcp.dev@posteo.de>
2023-07-19 22:20:39 +0200

    Revert "revert refactoring on ApiJsonToken type"
    This reverts commit 53ee2670f09414ea7b2d15f1fbd2b571c0e5e409.

--- src/types.rs
+++ src/types.rs
@@ -5,21 +5,19 @@ pub type Result<T> = std::result::Result<T, CarboneError>;
 
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct ApiJsonToken{
-    api_token: String,
-}
+pub struct ApiJsonToken(String);
 
 impl ApiJsonToken {
     pub fn new(s: String) -> Result<Self> {
         if s.len() >= 300 {
-            Ok(Self{api_token: s})
+            Ok(ApiJsonToken(s))
         } else {
             Err(CarboneError::Error("wrong token length".to_string()))
         }
     }
 
     pub fn as_str(&self) -> &str {
-        &self.api_token
+        &self.0
     }
 }
 
